a4ac969bd47c827ec320e29378a0c091549654cb
Park Jae Seong <timer973@naver.com>
2024-05-09 17:51:22 +0900

    변수 Section 완료

---
+++ src/variable/ex/VarEx3Question.java
@@ -0,0 +1,12 @@
+package variable.ex;
+
+public class VarEx3Question {
+
+    public static void main(String[] args) {
+        long longVar = 10000000000L;
+        System.out.println("longVar = " + longVar);
+
+        boolean booleanVar = true;
+        System.out.println("booleanVar = " + booleanVar);
+    }
+}
